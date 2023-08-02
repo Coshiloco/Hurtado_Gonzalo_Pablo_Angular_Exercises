b4d59986f1135966ff62a06755a7d982433303b0
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-08-02 19:53:13 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExerciseOneComponent } from './exercise-one/exercise-one.component';
+import { ExerciseTwoComponent } from './exercise-two/exercise-two.component';
+import { ExerciseThreeComponent } from './exercise-three/exercise-three.component';
+import { ExerciseFourComponent } from './exercise-four/exercise-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExerciseOneComponent,
+    ExerciseTwoComponent,
+    ExerciseThreeComponent,
+    ExerciseFourComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exercise-three/exercise-three.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-three',
+  templateUrl: './exercise-three.component.html',
+  styleUrls: ['./exercise-three.component.css']
+})
+export class ExerciseThreeComponent {
+
+}
