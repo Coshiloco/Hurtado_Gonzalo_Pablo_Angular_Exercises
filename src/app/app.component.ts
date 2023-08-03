0bbd421d8d281b30e47e6bf7dc4a66c6ee476499
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-08-03 13:59:40 +0200

    Doing Beutifull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './sections/header.css'],
 })
 export class AppComponent {
   title = 'Hurtado_Gonzalo_Pablo_Angular_Exercises';
+  public menuOpen: Boolean = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
