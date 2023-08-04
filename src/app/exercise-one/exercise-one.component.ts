218ff60aa0db17798814945f3d47a76b32eb2867
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-08-04 13:33:37 +0200

    ExerciseOneComplete

--- src/app/exercise-one/exercise-one.component.ts
+++ src/app/exercise-one/exercise-one.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-exercise-one',
   templateUrl: './exercise-one.component.html',
-  styleUrls: ['./exercise-one.component.css']
+  styleUrls: ['./exercise-one.component.css'],
 })
 export class ExerciseOneComponent {
-
+  name = '';
+  font = 'Arial';
+  size = 16;
+  align = 'left';
 }
